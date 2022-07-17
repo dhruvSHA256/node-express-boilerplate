9fd09aace7fe7e5d32278ba172056d29a40c695b
DHruv <dhruv.sha256@gmail.com>
2022-07-18 02:40:10 +0530

    added example routes and models

--- app.js
+++ app.js
@@ -1,39 +1,43 @@
 const express = require("express");
 const mongoose = require('mongoose');
+const exampleRoute = require("./routes/example");
+
+// const User = require("./models/user");
 
 const PORT = process.env.PORT || 5000;
 const DB_USER = process.env.DB_USER;
 const DB_PASS = process.env.DB_PASS;
 
-if (!DB_USER) {
-    console.error('[error]: The "DB_USER" environment variable is required');
-    process.exit(1);
-}
+// if (!DB_USER) {
+//     console.error(`[error]: The "DB_USER" environment variable is required`);
+//     process.exit(1);
+// }
 
-if (!DB_PASS) {
-    console.error('[error]: The "DB_PASS" environment variable is required');
-    process.exit(1);
-}
+// if (!DB_PASS) {
+//     console.error(`[error]: The "DB_PASS" environment variable is required`);
+//     process.exit(1);
+// }
 
-const MONGO_URL = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.gxenyiv.mongodb.net/?retryWrites=true&w=majority`;
-mongoose.connect(MONGO_URL,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
+// const MONGO_URL = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.gxenyiv.mongodb.net/?retryWrites=true&w=majority`;
+// mongoose.connect(MONGO_URL,
+//     {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//     });
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-const app = express()
+const app = express();
 
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-app.get('', (_req, res) => {
+app.get("", (_req, res) => {
     res.send({ msg: "hello world" });
 });
 
+app.use("/example", exampleRoute);
 
 app.listen(PORT);
